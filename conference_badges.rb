a8db20fb06869de533e2ed26a9dc84e8df4069df
Ryan O'Toole <otoolerj@gmail.com>
2018-03-22 05:32:43 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,6 +13,7 @@ end
 def assign_rooms(attendees)
   room_assignments = []
   attendees.each_with_index do |attendee, index|
-    puts "Hello, #{attendee}! You'll be assigned to room #{index}"
+    room_assignments.push(puts "Hello, #{attendee}! You'll be assigned to room #{index}")
   end
+  return room_assignments
 end
