7c19b50a54ec518a5c8f87f6361fbe4261252688
Ryan O'Toole <otoolerj@gmail.com>
2018-03-22 05:08:26 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,8 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
+  friendly_array = []
   attendees.each do |person|
-    badge_maker(person)
+    friendly_array.push(badge_maker(person))
   end
 end 
